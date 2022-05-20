c9ca154486c2829a0ca7a5f41bd48785893b603d
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-20 17:42:49 +0530

    Update song.py

--- song.py
+++ song.py
@@ -52,3 +52,44 @@ async def _stream_platforma(e):
    p = (f'({x.get("price")})' if "stream" not in x.get("price") else "") if x.get("price") else ""
    buttons.append([Button.url(x.get("name") + p, x.get("url"))])
  await e.reply(src, buttons=buttons)
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+@command(pattern='mdisk')
+async def _mdisk(e):
+ try:
+   _url = e.text.split(None, maxsplit=1)[1]
+ except IndexError:
+   return await e.reply("Please provide mdisk url.")
+ url = _url.split("/")
+ if len(url) == 0:
+   return await e.reply("Failed to find MediaID.")
+ url = url[-1]
+ base_url = "https://diskuploader.entertainvideo.com/v1/file/cdnurl?param=" + url
+ r = get(base_url)
+ try:
+     try:
+         r = r.json()
+     except:
+         return await e.reply('Resource not found, 404.')  
+     filename = r['filename'] if r.get("filename") else 'file0'
+     src = r['download'] if r.get("download") else (r.get("source") or "")
+     duration = int(r['duration']) if r.get("duration") else 0
+     size = int(r['size']) if r.get('size') else 0
+ except:
+     return await e.reply("Error: extractMetadata failed.")
+ strf_du = str(datetime.timedelta(seconds = duration))
+ size = sizeof_fmt(size)
+ _data = f'**FileName:** {filename}'
+ _data += f'\n**Size:** {size}' if size != '0B' else ""
+ _data += f'\n**Duration:** {strf_du}' if duration != 0 else ""
+ _data += f'\n**Mdisk URL:** {_url}'
+ buttons = [Button.url('Direct URL', src)]
+ await e.reply(_data, buttons=buttons)
+     
+     
