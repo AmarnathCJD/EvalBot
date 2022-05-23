f7e09e69c711ec51594bacc8dec092977e2d28bd
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-23 15:24:49 +0530

    Update song.py

--- song.py
+++ song.py
@@ -6,58 +6,145 @@ import re
 import time
 from telethon import Button
 import datetime 
-from mega import Mega
-
-_mega = Mega()
-mega = _mega.login()
-
-pattern = r'"([^"]*)"'
 
 
-async def download_song(query: str):
-    out = await bash("spotdl '{}'".format(query))
-    match = re.findall(pattern, out) if out != "" else []
-    if len(match) == 0:
-        return "", "No results found!"
-    return match[-1] + ".mp3", ""
+import os
+
+import yt_dlp
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
+from youtubesearchpython import VideosSearch as vs
+
+aud_ops = {
+    "format": "bestaudio",
+    "addmetadata": True,
+    "key": "FFmpegMetadata",
+    "prefer_ffmpeg": True,
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }
+    ],
+    "outtmpl": "%(id)s.mp3",
+    "quiet": True,
+    "logtostderr": False,
+}
+vid_ops = {
+    "format": "best[height>=720]",
+    "addmetadata": True,
+    "key": "FFmpegMetadata",
+    "prefer_ffmpeg": True,
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+    "outtmpl": "%(id)s.mp4",
+    "logtostderr": False,
+    "quiet": True,
+}
 
 
 @command(pattern="song")
-async def dl_song(e):
+async def download_song(e):
     try:
-        q = e.text.split(None, maxsplit=1)[1]
+        q = e.text.split(None, 1)[1]
     except IndexError:
-        return await e.reply("No query given!")
-    resp, err = await download_song(q)
-    if err != "":
-        return await e.reply(err)
-    async with e.client.action(e.chat_id, "audio"):
+        return await e.reply("The song query was not provided!")
+    try:
+        v = vs(q, limit=1).result()["result"][0]
+    except (IndexError, KeyError, TypeError):
+        return await e.reply("No song result found for your query!")
+    axe = await e.reply(
+        "Preparing to upload **{}** by {}".format(
+            v.get("title"), v.get("channel").get("name") or "Channel"
+        )
+    )
+    duration = int(v["duration"].split(":")[0]) * 60 + int(v["duration"].split(":")[1])
+    if duration > 3600:
+        await axe.edit("Upload failed song duration is more than 1 hour(s)!")
+    with yt_dlp.YoutubeDL(aud_ops) as yt:
         try:
-            await e.respond(file=resp)
-        except BaseException as errr:
-            await e.reply(str(errr))
+            yt.extract_info(v["link"])
+        except Exception as bx:
+            return await axe.edit(str(bx))
+    async with e.client.action(e.chat_id, "audio"):
+        await e.client.send_file(
+            e.chat_id,
+            v["id"] + ".mp3",
+            supports_streaming=False,
+            force_document=False,
+            allow_cache=False,
+            attributes=[
+                DocumentAttributeAudio(
+                    duration=duration,
+                    title=v["title"],
+                    performer=v["channel"]["name"],
+                    waveform=b"320",
+                )
+            ],
+        )
+    await axe.delete()
+    os.remove(v["id"] + ".mp3")
+
+
+@command(pattern="video")
+async def download_video(e):
+    try:
+        q = e.text.split(None, 1)[1]
+    except IndexError:
+        return await e.reply("The video query was not provided!")
+    try:
+        v = vs(q, limit=1).result()["result"][0]
+    except (IndexError, KeyError, TypeError):
+        return await e.reply("No video result found for your query!")
+    axe = await e.reply(
+        "Preparing to upload Video **{}** by {}".format(
+            v.get("title"), v.get("channel").get("name") or "Channel"
+        )
+    )
+    duration = int(v["duration"].split(":")[0]) * 60 + int(v["duration"].split(":")[1])
+    if duration > 3600:
+        await axe.edit("Upload failed video duration is more than 1 hour(s)!")
+    with yt_dlp.YoutubeDL(vid_ops) as yt:
+        try:
+            yt.extract_info(v["link"])
+        except Exception as bx:
+            return await axe.edit(str(bx))
+    async with e.client.action(e.chat_id, "video"):
+        await e.client.send_file(
+            e.chat_id,
+            v["id"] + ".mp4",
+            supports_streaming=True,
+            caption=v["title"],
+            attributes=[DocumentAttributeVideo(duration=duration, w=854, h=480)],
+        )
+    await axe.delete()
+    os.remove(v["id"] + ".mp4")
+
 
 @command(pattern="stream")
 async def _stream_platforma(e):
- try:
+  try:
         q = e.text.split(None, maxsplit=1)[1]
- except IndexError:
+  except IndexError:
         return await e.reply("No query given!")
- r = requests.get("https://api.roseloverx.tk/stream?q={}".format(quote(q)))
- try:
-  r = r.json()
- except:
-  return await e.reply("ErrorJsonDecoder.")
- src = "Streaming sites for **{}**:".format(q)
- buttons = []
- s = 0
- if not r["data"]:
-  return await e.reply(src+"\nNot available on any OTT.")
- for x in r["data"]:
-   s += 1
-   p = (f'({x.get("price")})' if "stream" not in x.get("price") else "") if x.get("price") else ""
-   buttons.append([Button.url(x.get("name") + p, x.get("url"))])
- await e.reply(src, buttons=buttons)
+  r = requests.get("https://api.roseloverx.tk/stream?q={}".format(quote(q)))
+  try:
+        r = r.json()
+  except:
+        return await e.reply("ErrorJsonDecoder.")
+  src = "Streaming sites for **{}**:".format(q)
+  buttons = []
+  s = 0
+  if not r["data"]:
+        return await e.reply(src+"\nNot available on any OTT.")
+  for x in r["data"]:
+         s += 1
+         p = (f'({x.get("price")})' if "stream" not in x.get("price") else "") if x.get("price") else ""
+         buttons.append([Button.url(x.get("name") + p, x.get("url"))])
+  await e.reply(src, buttons=buttons)
 
 def sizeof_fmt(num, suffix="B"):
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
@@ -110,27 +197,3 @@ async def _compress_vid(e):
  _f= await e.respond(file='compressed-' + vd)
  comp_size = sizeof_fmt(_f.file.size)
  await e.reply('Time: ' + str(time.time() - t) + f's\nFileName: `compressed-{vd}`' + f'\n**{sizeof_fmt(v.file.size)}** --> **{comp_size}**')
-
-@command(pattern='upload')
-async def _upload_to_meganz(e):
- if e.reply_to:
-      _f = await e.get_reply_message()
-      _file = await _f.download_media()
-      _type = 'file'
- else:
-      try:
-        _f = e.text.split(None, maxsplit=1)[1]
-      except IndexError:
-        return await e.reply('Reply to a file/ url.')
-      _file = _f
-      _type = 'url'
- if _type == 'file':
-   file = mega.upload(_file)
-   _link = mega.get_upload_link(file)
- else:
-   _link = 'soon!'
- _disk = mega.get_storage_space(giga=True)
- _result = 'Uploaded To Mega.NZ'
- _result += 'URL: {}'.format(_link)
- _result += 'DiskSpace: {}/20GB'.format(_disk)
- await e.reply(_result)
