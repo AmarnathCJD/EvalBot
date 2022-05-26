1f98358b9c856d7478b8222111f21d2926627182
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-26 20:20:38 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -178,7 +178,7 @@ async def download_video(url: str, quality: str):
         f = await bash(FORMATS_CMD)
         vitag = [x.split()[0] for x in f.splitlines() if "1080" in x][-1]
         aitag = [x.split()[0] for x in f.splitlines() if "opus" in x][-1]
-        ID = url.split("/")[-1]
+        ID = url.split("=")[-1]
         DIR = "/{}/".format(ID)
         try:
             os.mkdir(DIR)
