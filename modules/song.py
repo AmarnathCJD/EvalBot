0efb455b73089241ca8f76c39dc9a6482ae06145
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-26 21:39:15 +0530

    Create song.py

--- modules/song.py
+++ modules/song.py
@@ -178,16 +178,22 @@ async def _dl_hd(e):
         v = e.text.split(maxsplit=1)[1]
     except:
         return await e.reply("No URL given")
+    q = v.split("-")
+    if len(q) == 1:
+       q = "1080"
+    else:
+       q = q[1]
+       v = v.replace(q)
     try:
         vd = vs(v, limit=1).result()["result"][0]
     except (IndexError, KeyError, TypeError):
         return await e.reply("No song result found for your query!")
     axe = await e.reply(
-        "Preparing to upload Video **{}**\nQuality: {}".format(vd.get("title"), Quality)
+        "Preparing to upload Video **{}**\nQuality: {}".format(vd.get("title"), q)
     )
-    v = await download_video(vd["link"], "1080")
+    v = await download_video(vd["link"], q)
     if v == "err":
-        await e.reply("Queries Quality is not available for this video.")
+        await e.reply("Quality is not available for this video.")
     thumb_url = vd["thumbnails"][0]["url"]
     with open("thumb.jpg", "wb") as t:
         t.write(requests.get(thumb_url).content)
@@ -197,24 +203,26 @@ async def _dl_hd(e):
 
 
 async def download_video(url: str, quality: str):
-    FORMATS_CMD = "yt-dlp {} -F --write-thumbnail".format(url)
-    f = await bash(FORMATS_CMD)
-    try:
-        vitag = [x.split()[0] for x in f.splitlines() if quality in x][-1]
-        aitag = [x.split()[0] for x in f.splitlines() if "opus" in x][-1]
-    except:
-        return "err"
-    ID = url.split("=")[-1]
-    DIR = "./{}/".format(ID)
-    try:
-        os.mkdir(DIR)
-    except OSError:
-        pass
-    await bash("yt-dlp {} -f {} -o {}video.mp4".format(url, vitag, DIR))
-    await bash("yt-dlp {} -f {} -o {}audio.opus".format(url, aitag, DIR))
-    Ffmpeg_Merge = "ffmpeg -i {}video.mp4 -i {}audio.opus -c copy {}output.mkv".format(
-        DIR, DIR, DIR
-    )
-    print(Ffmpeg_Merge)
-    await bash(Ffmpeg_Merge)
-    return DIR + "output.mkv"
+        FORMATS_CMD = "yt-dlp {} -F --write-thumbnail".format(url)
+        f = await bash(FORMATS_CMD)
+        try: 
+         vitag = [x.split()[0] for x in f.splitlines() if quality in x][-1]
+         aitag = [x.split()[0] for x in f.splitlines() if "opus" in x][-1]
+        except:
+         return "err"
+        ID = url.split("=")[-1]
+        DIR = "./{}/".format(ID)
+        try:
+            os.mkdir(DIR)
+        except OSError:
+            pass
+        await bash("yt-dlp {} -f {} -o {}video.mp4".format(url, vitag, DIR))
+        await bash("yt-dlp {} -f {} -o {}audio.opus".format(url, aitag, DIR))
+        Ffmpeg_Merge = (
+            "ffmpeg -i {}video.mp4 -i {}audio.opus -c copy {}output.mkv".format(
+                DIR, DIR, DIR
+            )
+        )
+        print(Ffmpeg_Merge)
+        await bash(Ffmpeg_Merge)
+        return DIR + "output.mkv"
