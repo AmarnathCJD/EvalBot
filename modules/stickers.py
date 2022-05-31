69dd52518591b3e665305051789f46cc8bff70a8
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-05-31 17:00:18 +0530

    ok

--- modules/stickers.py
+++ modules/stickers.py
@@ -48,8 +48,8 @@ async def _resize(e):
     os.remove(filename)
     os.remove(image)
 
-GIF_TO_WEBM = "ffmpeg -i {} -c vp9 -b:v 0 -crf 40 -vf scale=512:512 -t 00:00:03 {}"
-
+GIF_TO_WEBM = "ffmpeg -i {} -c vp9 -b:v 0 -crf 40 -vf scale={}:{} -t 00:00:03 {}"
+VID_DIMENTIONS = "ffprobe -v error -show_entries stream=width,height -of default=noprint_wrappers=1 {}"
 
 @command(pattern="webm")
 async def _gif_to_webm(e):
@@ -58,7 +58,15 @@ async def _gif_to_webm(e):
         return await e.reply("Reply to any GIF")
     gif = await i.download_media()
     filename = "".join(gif.split(".")[:1]) + ".webm"
-    r = await bash(GIF_TO_WEBM.format(gif, filename))
+    v = await bash(VID_DIMENTIONS.format(gif))
+    v = v.split("\n")[0].split(" ")
+    v = (int(v[0]), int(v[1]))
+    ratio = v[0] / v[1]
+    if ratio > 1:
+        v = (int(512 * ratio), 512)
+    else:
+        v = (512, int(512 / ratio))
+    r = await bash(GIF_TO_WEBM.format(gif, v[0], v[1], filename))
     await e.reply(file=filename)
     os.remove(filename)
     os.remove(gif)
