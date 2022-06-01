fb062ec4ebb6a7851d5f9c9ab3e3c4881f941786
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-01 06:11:43 +0000

    RoseLoverX: auto fixes

--- modules/imdb.py
+++ modules/imdb.py
@@ -1,6 +1,4 @@
 import os
-from platform import release
-from urllib.parse import quote
 
 from bs4 import BeautifulSoup
 from requests import get
@@ -136,8 +134,7 @@ def get_crew_cast_info(soup):
     if rev:
         user_review = rev.find(class_="ipc-html-content-inner-div").text
     story = ""
-    story_line = soup.find(
-        class_="ipc-page-section ipc-page-section--base celwidget")
+    story_line = soup.find(class_="ipc-page-section ipc-page-section--base celwidget")
     if story_line:
         story = story_line.find(class_="ipc-html-content-inner-div")
         if story:
@@ -163,8 +160,7 @@ def get_crew_cast_info(soup):
     aka = ""
     aka_ = soup.find({"data-testid": "title-details-akas"})
     if aka_:
-        aka = aka_.find(
-            "a", class_="ipc-metadata-list-item__list-content-item").text
+        aka = aka_.find("a", class_="ipc-metadata-list-item__list-content-item").text
     return {
         "cast": cast,
         "creators": creators,
@@ -281,8 +277,8 @@ async def display_movie(e, result_id):
     res = r.json()
     runtime = res["runtime"]
     tagline = res["tagline"]
-    imdb_id = res["imdb_id"]
-    release_date = res["release_date"]
+    res["imdb_id"]
+    res["release_date"]
     status = res["status"]
     if tagline:
         tagline = f"       -`{tagline}`"
@@ -293,7 +289,7 @@ async def display_movie(e, result_id):
     POSTER = f"https://image.tmdb.org/t/p/original{res['poster_path']}"
     await e.reply(
         f"**Added __{res['title']}__  to watched List**\n{tagline}\n{status}\n{watchtime}",
-        file=POSTER
+        file=POSTER,
     )
 
 
