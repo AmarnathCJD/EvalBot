191c78138ee2e81ea98f183f7f713e76487d84c7
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-06-01 12:55:02 +0530

    oki

--- modules/imdb.py
+++ modules/imdb.py
@@ -134,7 +134,8 @@ def get_crew_cast_info(soup):
     if rev:
         user_review = rev.find(class_="ipc-html-content-inner-div").text
     story = ""
-    story_line = soup.find(class_="ipc-page-section ipc-page-section--base celwidget")
+    story_line = soup.find(
+        class_="ipc-page-section ipc-page-section--base celwidget")
     if story_line:
         story = story_line.find(class_="ipc-html-content-inner-div")
         if story:
@@ -160,7 +161,8 @@ def get_crew_cast_info(soup):
     aka = ""
     aka_ = soup.find({"data-testid": "title-details-akas"})
     if aka_:
-        aka = aka_.find("a", class_="ipc-metadata-list-item__list-content-item").text
+        aka = aka_.find(
+            "a", class_="ipc-metadata-list-item__list-content-item").text
     return {
         "cast": cast,
         "creators": creators,
@@ -266,7 +268,8 @@ async def display_tv_series(e, result_id):
     else:
         tagline = ""
     s = add_series(
-        e.chat_id, result_id, res["name"], get_watchtime(runtime, episodes, True)
+        e.chat_id, result_id, res["name"], get_watchtime(
+            runtime, episodes, True)
     )
     watchtime = f"**Watchtime**: +{get_watchtime(runtime, episodes)}"
     if s:
@@ -277,6 +280,7 @@ async def display_tv_series(e, result_id):
     await e.reply(
         f"**Added __{res['name']}__  to watched List**\n{tagline}\n{status}\n{seasons}\n{watchtime}",
         file=POSTER,
+        force_document=True,
     )
 
 
@@ -295,7 +299,8 @@ async def display_movie(e, result_id):
         tagline = f"       -`{tagline}`"
     else:
         tagline = ""
-    s = add_series(e.chat_id, result_id, res["title"], get_watchtime(runtime, 1, True))
+    s = add_series(e.chat_id, result_id,
+                   res["title"], get_watchtime(runtime, 1, True))
     if s:
         return await e.reply(
             "Already in watched list!\n" f"**Title**: {res['title']}\n"
@@ -309,6 +314,7 @@ async def display_movie(e, result_id):
     await e.reply(
         f"**Added __{res['title']}__  to watched List**\n{tagline}\n{status}\n{release_date}\n{imdb_id}\n{watchtime}",
         file=POSTER,
+        force_document=True,
     )
 
 
