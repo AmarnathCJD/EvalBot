b91baca60341eb9affb465cd6b744db977cf2e70
Amarnathcjd <amarnathcharichilil@gmail.com>
2022-06-01 12:21:46 +0530

    ok

--- modules/imdb.py
+++ modules/imdb.py
@@ -1,7 +1,6 @@
 import os
 
 from bs4 import BeautifulSoup
-from numpy import imag
 from requests import get
 
 from ._db import DB
@@ -135,7 +134,8 @@ def get_crew_cast_info(soup):
     if rev:
         user_review = rev.find(class_="ipc-html-content-inner-div").text
     story = ""
-    story_line = soup.find(class_="ipc-page-section ipc-page-section--base celwidget")
+    story_line = soup.find(
+        class_="ipc-page-section ipc-page-section--base celwidget")
     if story_line:
         story = story_line.find(class_="ipc-html-content-inner-div")
         if story:
@@ -161,7 +161,8 @@ def get_crew_cast_info(soup):
     aka = ""
     aka_ = soup.find({"data-testid": "title-details-akas"})
     if aka_:
-        aka = aka_.find("a", class_="ipc-metadata-list-item__list-content-item").text
+        aka = aka_.find(
+            "a", class_="ipc-metadata-list-item__list-content-item").text
     return {
         "cast": cast,
         "creators": creators,
