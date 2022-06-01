d393378ad1c9cd420a4d88ed61bf22c188feec2c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-01 08:10:45 +0000

    RoseLoverX: auto fixes

--- modules/imdb.py
+++ modules/imdb.py
@@ -135,8 +135,7 @@ def get_crew_cast_info(soup):
     if rev:
         user_review = rev.find(class_="ipc-html-content-inner-div").text
     story = ""
-    story_line = soup.find(
-        class_="ipc-page-section ipc-page-section--base celwidget")
+    story_line = soup.find(class_="ipc-page-section ipc-page-section--base celwidget")
     if story_line:
         story = story_line.find(class_="ipc-html-content-inner-div")
         if story:
@@ -162,8 +161,7 @@ def get_crew_cast_info(soup):
     aka = ""
     aka_ = soup.find({"data-testid": "title-details-akas"})
     if aka_:
-        aka = aka_.find(
-            "a", class_="ipc-metadata-list-item__list-content-item").text
+        aka = aka_.find("a", class_="ipc-metadata-list-item__list-content-item").text
     return {
         "cast": cast,
         "creators": creators,
@@ -286,8 +284,7 @@ async def display_tv_series(e, result_id):
     else:
         tagline = ""
     s = add_series(
-        e.sender_id, result_id, res["name"], get_watchtime(
-            runtime, episodes, True)
+        e.sender_id, result_id, res["name"], get_watchtime(runtime, episodes, True)
     )
     watchtime = f"**Watchtime**: +{get_watchtime(runtime, episodes)}"
     if s:
@@ -395,8 +392,7 @@ async def _rmwatched(e):
     await e.reply(
         text,
         buttons=[
-            Button.inline(
-                "Yes", data="rmwatched_yes_{}".format(s["series_id"])),
+            Button.inline("Yes", data="rmwatched_yes_{}".format(s["series_id"])),
             Button.inline("No", data="rmwatched_no_{}".format(s["series_id"])),
         ],
     )
