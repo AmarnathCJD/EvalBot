bdfcc7c20a03605106fddbf4e144b483fbd8efb4
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-09 03:29:08 +0000

    RoseLoverX: auto fixes

--- modules/inline.py
+++ modules/inline.py
@@ -1,15 +1,29 @@
 from config import bot
+from requests import get
 from telethon import events
-from requests import get 
+
 
 @bot.on(events.InlineQuery(pattern="url"))
 async def _url(e):
- try:
-   url = e.text.split(" ", maxsplit=3)[2]
- except IndexError:
-   return await e.answer([e.builder.article(title="No URL Given.", description="please provide a url to get its redirect output.", text="No url was given, provide it get redirect url.")])
- try:
-   r = get(url, allow_redirects=True)
- except Exception as a:
-   return await e.answer([e.builder.article(title="Error", description="URL unreachable.", text=str(a))])
- 
+    try:
+        url = e.text.split(" ", maxsplit=3)[2]
+    except IndexError:
+        return await e.answer(
+            [
+                e.builder.article(
+                    title="No URL Given.",
+                    description="please provide a url to get its redirect output.",
+                    text="No url was given, provide it get redirect url.",
+                )
+            ]
+        )
+    try:
+        r = get(url, allow_redirects=True)
+    except Exception as a:
+        return await e.answer(
+            [
+                e.builder.article(
+                    title="Error", description="URL unreachable.", text=str(a)
+                )
+            ]
+        )
