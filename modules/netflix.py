cd982f71dfcdc7c108e62369ee850bc2b35c7781
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-12 20:21:09 +0530

    Update netflix.py

--- modules/netflix.py
+++ modules/netflix.py
@@ -81,6 +81,11 @@ async def enter_details(email: str, password: str, browser):
         PASSWORD_TAG.send_keys(Keys.ENTER)
     except:
         pass
+    await asyncio.sleep(1.27)
+    try:
+        browser.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/button").click()
+    except:
+        pass
     await asyncio.sleep(0.27)
 
 
