727f1d00c99f466555f91d2323204a3ffebdb000
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-12 09:27:51 +0000

    RoseLoverX: auto fixes

--- modules/netflix.py
+++ modules/netflix.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def browser():
-  chrome_options = Options()
-  chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  return webdriver.Chrome(chrome_options=chrome_options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    return webdriver.Chrome(chrome_options=chrome_options)
