b3080fa0be1f203a81476dd02eddbcb120fa556f
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-12 19:56:20 +0530

    Update netflix.py

--- modules/netflix.py
+++ modules/netflix.py
@@ -1,10 +1,12 @@
 import asyncio
 import base64
 import io
+import string
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.keys import Keys
 
 def setup_browser(new=False):
     chrome_options = Options()
@@ -16,7 +18,6 @@ def setup_browser(new=False):
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     b = webdriver.Chrome(chrome_options=chrome_options)
     return b
-
 
 def xpath(element):
     components = []
@@ -33,47 +34,38 @@ def xpath(element):
     components.reverse()
     return "/%s" % "/".join(components)
 
+def gen_email():
+    CHAR = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(CHAR) for a in range(10)) + '@gmail.com'
 
-def enter_email(email: str, browser):
+async def enter_details(email: str, password: str, browser):
     EMAIL_TAG = browser.find_element_by_xpath(
         "/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div[2]/form/div/ul/li/div/div/label/input"
     )
     EMAIL_TAG.clear()
     EMAIL_TAG.send_keys(email)
-    if EMAIL_TAG.get_attribute("value") != "":
-        return True
-    return False
-
-
-def get_started(browser):
-    BTN = browser.find_element_by_xpath(
+    browser.find_element_by_xpath(
         "/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div[2]/form/div/div/button"
-    )
-    if BTN:
-        BTN.click()
-
-
-def click_next(browser):
-    NXT = browser.find_element_by_xpath(
+    ).click()
+    await asyncio.sleep(0.27)
+    browser.find_element_by_xpath(
         "/html/body/div[1]/div/div/div[2]/div/div[2]/button"
-    )
-    if NXT:
-        NXT.click()
-
+    ).click()
+    await asyncio.sleep(0.27)
+    PASSWORD_TAG = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/form/div/div[1]/div[2]/ul/li[2]/div/div/label/input')
+    PASSWORD_TAG.clear()
+    PASSWORD_TAG.send_keys(password)
+    PASSWORD_TAG.send_keys(Keys.ENTER)
+    await asyncio.sleep(0.27)
 
 async def send_photo(browser, e):
     with io.BytesIO(base64.b64decode(browser.get_screenshot_as_base64())) as f:
         f.name = "screenshot.png"
         await e.respond(file=f)
 
-
-async def setup_netflix(e, email=""):
+async def setup_netflix(email=""):
+    _email = gen_email() if email == "" else email
     browser = setup_browser()
     browser.get("https://netflix.com")
-    if email != "":
-        enter_email(email, browser)
-        get_started(browser)
-        await asyncio.sleep(0.27)
-        click_next(browser)
-        await asyncio.sleep(0.27)
+    await enter_details(_email, "qwerty123#", browser)
     return browser
