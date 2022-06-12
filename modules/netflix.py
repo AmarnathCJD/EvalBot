c5698fe859d12950eeaf09cbcb0d9cecf7a9f8f0
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-12 15:25:15 +0530

    Update netflix.py

--- modules/netflix.py
+++ modules/netflix.py
@@ -1,13 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+CHROME = []
 
 
-def browser():
+def browser(new=False):
+    if not new and len(CHROME) != 0:
+       return CHROME[0]
+    CHROME.empty()
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    return webdriver.Chrome(chrome_options=chrome_options)
+    b = webdriver.Chrome(chrome_options=chrome_options)
+    CHROME.append(b)
+    return b
 
 
 def xpath(element):
@@ -24,3 +32,9 @@ def xpath(element):
         child = parent
     components.reverse()
     return "/%s" % "/".join(components)
+
+def enter_email(email: str, browser):
+    EMAIL_TAG = browser.find_element_by_xpath('/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div[2]/form/div/ul/li/div/div/label/input')
+    EMAIL_TAG.clear()
+    EMAIL_TAG.send_keys(email)
+    return EMAIL_TAG.text or "Failed to enter email."
