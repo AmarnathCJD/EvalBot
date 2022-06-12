5cbeec7c4535ae3270b67831570eaf7d6de2bfca
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-12 14:39:21 +0000

    RoseLoverX: auto fixes

--- modules/netflix.py
+++ modules/netflix.py
@@ -58,28 +58,28 @@ async def enter_details(email: str, password: str, browser):
     EMAIL_TAG.clear()
     EMAIL_TAG.send_keys(email)
     try:
-     browser.find_element_by_xpath(
-        "/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div[2]/form/div/div/button"
-    ).click()
+        browser.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div[2]/form/div/div/button"
+        ).click()
     except:
-     pass
+        pass
     await asyncio.sleep(0.27)
     try:
-      browser.find_element_by_xpath(
-        "/html/body/div[1]/div/div/div[2]/div/div[2]/button"
-    ).click()
+        browser.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[2]/div/div[2]/button"
+        ).click()
     except:
-      pass
+        pass
     await asyncio.sleep(0.27)
     try:
-     PASSWORD_TAG = browser.find_element_by_xpath(
-        "/html/body/div[1]/div/div/div[2]/div/form/div/div[1]/div[2]/ul/li[2]/div/div/label/input"
-    )
-     PASSWORD_TAG.clear()
-     PASSWORD_TAG.send_keys(password)
-     PASSWORD_TAG.send_keys(Keys.ENTER)
+        PASSWORD_TAG = browser.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[2]/div/form/div/div[1]/div[2]/ul/li[2]/div/div/label/input"
+        )
+        PASSWORD_TAG.clear()
+        PASSWORD_TAG.send_keys(password)
+        PASSWORD_TAG.send_keys(Keys.ENTER)
     except:
-      pass
+        pass
     await asyncio.sleep(0.27)
 
 
