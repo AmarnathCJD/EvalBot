39fda889e9f56b4590a5425f9d7141dc824706c9
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-12 14:26:41 +0000

    RoseLoverX: auto fixes

--- modules/netflix.py
+++ modules/netflix.py
@@ -1,12 +1,13 @@
 import asyncio
 import base64
 import io
-import string
 import random
+import string
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+
 
 def setup_browser(new=False):
     chrome_options = Options()
@@ -18,6 +19,7 @@ def setup_browser(new=False):
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     b = webdriver.Chrome(chrome_options=chrome_options)
     return b
+
 
 def xpath(element):
     components = []
@@ -34,9 +36,11 @@ def xpath(element):
     components.reverse()
     return "/%s" % "/".join(components)
 
+
 def gen_email():
     CHAR = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(CHAR) for a in range(10)) + '@gmail.com'
+    return "".join(random.choice(CHAR) for a in range(10)) + "@gmail.com"
+
 
 async def enter_details(email: str, password: str, browser):
     EMAIL_TAG = browser.find_element_by_xpath(
@@ -52,16 +56,20 @@ async def enter_details(email: str, password: str, browser):
         "/html/body/div[1]/div/div/div[2]/div/div[2]/button"
     ).click()
     await asyncio.sleep(0.27)
-    PASSWORD_TAG = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/form/div/div[1]/div[2]/ul/li[2]/div/div/label/input')
+    PASSWORD_TAG = browser.find_element_by_xpath(
+        "/html/body/div[1]/div/div/div[2]/div/form/div/div[1]/div[2]/ul/li[2]/div/div/label/input"
+    )
     PASSWORD_TAG.clear()
     PASSWORD_TAG.send_keys(password)
     PASSWORD_TAG.send_keys(Keys.ENTER)
     await asyncio.sleep(0.27)
 
+
 async def send_photo(browser, e):
     with io.BytesIO(base64.b64decode(browser.get_screenshot_as_base64())) as f:
         f.name = "screenshot.png"
         await e.respond(file=f)
+
 
 async def setup_netflix(email=""):
     _email = gen_email() if email == "" else email
