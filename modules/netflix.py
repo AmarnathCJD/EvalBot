a8694a2d47735d03e1b29c3731eda84c4917f545
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-12 15:16:33 +0530

    Update netflix.py

--- modules/netflix.py
+++ modules/netflix.py
@@ -8,3 +8,18 @@ def browser():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(chrome_options=chrome_options)
+
+def xpath(element):
+    components = []
+    child = element if element.name else element.parent
+    for parent in child.parents:  # type: bs4.element.Tag
+        siblings = parent.find_all(child.name, recursive=False)
+        components.append(
+            child.name if 1 == len(siblings) else '%s[%d]' % (
+                child.name,
+                next(i for i, s in enumerate(siblings, 1) if s is child)
+                )
+            )
+        child = parent
+    components.reverse()
+    return '/%s' % '/'.join(components)
